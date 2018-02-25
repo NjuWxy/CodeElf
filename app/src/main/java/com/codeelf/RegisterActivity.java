7fe81f144d58e4fd36d4585837cd9f6eaef5233a
151250148 <151250148@smail.nju.edu.cn>
2018-02-25 14:09:28 +0800

    创建register页面

---
+++ app/src/main/java/com/codeelf/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.codeelf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
+}
