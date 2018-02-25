78aa13c6634b77d27373321be6c6c67a2f905d5e
151250148 <151250148@smail.nju.edu.cn>
2018-02-25 14:31:52 +0800

    init commit

--- app/src/main/java/com/codeelf/LoginActivity.java
+++ app/src/main/java/com/codeelf/LoginActivity.java
@@ -3,11 +3,11 @@ package com.codeelf;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
     }
 }
